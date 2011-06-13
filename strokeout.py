44694bb9d84fe04718f01b31c96c08170f6c1de6
Jeff Browne <jbrowne@cs.ucsb.edu>
2011-06-13 15:07:37 -0700

    Error allowed for square filling.

--- strokeout.py
+++ strokeout.py
@@ -8,6 +8,14 @@ import pdb
 FILLEDVAL = 0.0 
 COLORFACTOR = 10
 
+DENOISE_K = 5
+SMOOTH_K = 23
+INV_FACTOR = 0.5
+INK_THRESH = 122
+
+PRUNING_FACTOR = 0.8
+SQUARE_ERROR = 0.2
+PRUNING_FACTOR = (1 - PRUNING_FACTOR) * SQUARE_ERROR + PRUNING_FACTOR 
 
 #***************************************************
 #Square filling + helper functions
@@ -57,7 +65,8 @@ def fillSquares(startPoint, img, squareFill=255):
             squareSeeds.append(((x, p1y-1), center)) #Add the top/bottom perimeter points
             squareSeeds.append(((x, p2y+1), center))
             for y in range(p1y, p2y+1):
-               setImgVal(x,y,squareFill,img)  #Don't match any of these points next time
+               if isValidPoint((x,y), img):
+                  setImgVal(x,y,squareFill,img)  #Don't match any of these points next time
 
          for y in range(p1y, p2y+1): #Add the right/left perimeter points
             squareSeeds.append(((p1x-1, y), center))
@@ -96,20 +105,22 @@ def isValidPoint(point, img):
 
 def isValidSquare(square, img):
    """Check to see if square is unbroken"""
+   global SQUARE_ERROR
+   errorThresh  = SQUARE_ERROR
    p1, p2 = square
    p1x, p1y = square[0] 
    p2x, p2y = square[1] 
+   size = 1 + (p2x - p1x)
 
-   if not isValidPoint(p1, img) or not isValidPoint(p2, img):
-      #***INVALID square
-      return False
-
-
+   maxInvalidPixels = int( errorThresh * (size * size) )
+   badPixels = 0
    for x in range(p1x, p2x+1):
       for y in range(p1y, p2y+1):
          if not isValidPoint((x,y), img):
-            #INVALID square
-            return False
+            badPixels += 1
+            if badPixels > maxInvalidPixels:
+               #INVALID square
+               return False
 
    #VALID
    return True
@@ -129,7 +140,7 @@ def growSquare(point, img, directions = ['nw','ne','sw','se']):
    while len(squareStack) > 0:
       curSquare = squareStack.pop()
 
-      curSize = curSquare[1][1] - curSquare[0][1] #Y difference = size
+      curSize = curSquare[1][1] + 1 - curSquare[0][1] #Y difference = size
       if curSize > maxSize:
          maxSquare = tuple(curSquare)
          maxSize = curSize
@@ -138,10 +149,11 @@ def growSquare(point, img, directions = ['nw','ne','sw','se']):
       p2x, p2y = list(curSquare[1])
 
       testSquares = {}
-      testSquares['nw'] = ( (p1x-1, p1y-1), (p2x, p2y) ) #NorthWest
-      testSquares['ne'] = ( (p1x, p1y-1), (p2x+1, p2y) ) #NorthEast
-      testSquares['sw'] = ( (p1x-1, p1y), (p2x, p2y+1) ) #SouthWest
-      testSquares['se'] = ( (p1x, p1y), (p2x+1, p2y+1) ) #SouthEast
+      growth = (curSize + 1) / 2
+      testSquares['nw'] = ( (p1x-growth, p1y-growth), (p2x, p2y) ) #NorthWest
+      testSquares['ne'] = ( (p1x, p1y-growth), (p2x+growth, p2y) ) #NorthEast
+      testSquares['sw'] = ( (p1x-growth, p1y), (p2x, p2y+growth) ) #SouthWest
+      testSquares['se'] = ( (p1x, p1y), (p2x+growth, p2y+growth) ) #SouthEast
 
       for dir, newSquare in testSquares.items():
          if dir in directions and isValidSquare(newSquare, img):
@@ -170,14 +182,18 @@ class Stroke(object):
       """Merge two strokes together"""
       self.points.extend(rhsStroke.points)
 
-def smooth(img, ksize = 9):
+def smooth(img, ksize = 9, type='median'):
    """Do a median smoothing with kernel size ksize"""
    retimg = cv.CloneMat(img)
+   if type == 'gauss':
+      smoothtype = cv.CV_GAUSSIAN
+   else:
+      smoothtype = cv.CV_MEDIAN
    #retimg = cv.CreateMat(img.cols, img.rows, cv.CV_8UC3)
    #                            cols, rows, anchorx,y, shape
    #kernel = cv.CreateStructuringElementEx(3,3, 1,1, cv.CV_SHAPE_RECT,
                                           #(0,1,0,1,0,1,0,1,0))
-   cv.Smooth(img, retimg, smoothtype= cv.CV_MEDIAN, param1=ksize, param2=ksize)
+   cv.Smooth(img, retimg, smoothtype= smoothtype, param1=ksize, param2=ksize)
    return retimg
 
 def invert (cv_img):
@@ -189,15 +205,23 @@ def invert (cv_img):
 
 def removeBackground(cv_img):
    """Take in a color image and convert it to a binary image of just ink"""
+   #Hardcoded for resolution/phone/distance
+   denoise_k = 5
+   smooth_k = 23
+   inv_factor = 0.5
+   ink_thresh = 122
    bg_img = cv.CreateMat(cv_img.rows, cv_img.cols, cv.CV_8UC1)
    cv.CvtColor(cv_img, bg_img, cv.CV_RGB2GRAY)
-   inv_img = smooth(bg_img, ksize=23)
-   bg_img = smooth(bg_img, ksize=5)
-   #bg_img = cv.CloneImage(cv_img)
+   inv_img = smooth(bg_img, ksize=smooth_k)
+   #show(inv_img)
+   bg_img = smooth(bg_img, ksize=denoise_k)
+   #show(bg_img)
   
    inv_img = invert(inv_img)
-   cv.AddWeighted(bg_img, 0.5, inv_img, 0.5, 0.0,bg_img )
-   cv.Threshold(bg_img, bg_img, 123, 255, cv.CV_THRESH_BINARY)
+   cv.AddWeighted(bg_img, inv_factor, inv_img, (1 - inv_factor), 0.0,bg_img )
+   #show(bg_img)
+   cv.Threshold(bg_img, bg_img, ink_thresh, 255, cv.CV_THRESH_BINARY)
+   #show(bg_img)
 
 
    return bg_img
@@ -219,13 +243,15 @@ def squareSize(square):
 def processStrokes(cv_img):
    """Take in a raw, color image and return a list of strokes extracted from it."""
    global DEBUGIMG
+
    temp_img = removeBackground(cv_img)
 
+   DEBUGIMG = temp_img #cv.CloneMat(temp_img)
+   #cv.Set(DEBUGIMG, 255)
 
-   DEBUGIMG = cv.CloneMat(temp_img)
-   cv.Set(DEBUGIMG, 255)
 
-   strokelist = bitmapToUnorderedStrokes(temp_img,step = 1)
+   strokelist = bitmapToStrokes(temp_img,step = 1)
+   #show(DEBUGIMG)
    
    for s in strokelist:
       prev = None
@@ -237,7 +263,8 @@ def processStrokes(cv_img):
    return temp_img
 
 def pruneSquareTree(squareTree):
-   threshold = 0.8 #how close a fit to all of the blobs
+   global PRUNING_FACTOR
+   threshold = PRUNING_FACTOR #how close a fit to all of the blobs
    availSizes = {}
    totalSize = 0
    for node in squareTree.values():
@@ -298,14 +325,17 @@ def pruneSquareTree(squareTree):
          if k not in seen:
             procStack.append((k, linkParent))
 
-   print "Pruned square tree from %s to %s points at %s%% accuracy" % (len(squareTree), len(retTree), 100 * threshold)
+   #print "Pruned square tree from %s to %s points at %s%% accuracy" % (len(squareTree), len(retTree), 100 * threshold)
    return retTree
 
 def squareTreeToStrokes(squareTree):
+   global DEBUGIMG
 
    retStrokes = []
    tempTree = pruneSquareTree(squareTree)
+   preTree = dict(tempTree)
    #tempTree = dict(squareTree) #Copy the orig tree
+   totPoints = 0
    while len(tempTree) > 0:
       #Find a good leaf
       maxLeaf = tempTree.keys()[0]
@@ -339,9 +369,13 @@ def squareTreeToStrokes(squareTree):
             if kid not in seen and kid in tempTree:
                pathStack.append( curPath + [kid] )
 
+
       #Make it a stroke
       newStroke = Stroke()
+      nextTree = {}
       for point in tempTree.keys():
+         if point == (89, 1191):
+            pdb.set_trace()
          node = tempTree[point]
          newkids = []
          for kid in node['kids']:
@@ -350,18 +384,32 @@ def squareTreeToStrokes(squareTree):
 
          if len(newkids) > 0:
             node['kids'] = newkids
-         else:
-            del(tempTree[point])
+            nextTree[point] = node
+         #else:
+         #   del(tempTree[point])
+      tempTree = nextTree
 
       for point in maxPath:
          newStroke.addPoint(point)
-
+      totPoints += len(newStroke.points)
       retStrokes.append(newStroke)
 
-   print "Blob completed in %s strokes" % (len (retStrokes))
+   allPoints = []
+   for s in retStrokes:
+      allPoints.extend(s.points)
+
+   for p in preTree.keys():
+      if p not in allPoints:
+         print "%s in tree but not covered by strokes" % (str(p))
+         #cv.Circle(DEBUGIMG, p, 1, 0x0, thickness=3)
+
+   #print "Blob completed in %s strokes" % (len (retStrokes))
    return retStrokes
 
 def debugSquareTreeToStrokes(squareTree):
+   print "*** USING DEBUG TREE TO STROKES ***"
+   
+   squareTree = pruneSquareTree(squareTree)
    retStrokes = []
    for p, node in squareTree.items():
       for kid in node['kids']:
@@ -370,175 +418,15 @@ def debugSquareTreeToStrokes(squareTree):
          newStroke.addPoint( kid )
          retStrokes.append(newStroke)
 
+   print "*** USING DEBUG TREE TO STROKES ***"
+
    return retStrokes
 
-def squaresToStrokes(squareTree):
-   """Take in a bunch of squares and output one or more strokes approximating them"""
-   strokes = squareTreeToStrokes(squareTree)
-   #strokes = debugSquareTreeToStrokes(squareTree)
 
-   """
-   squares = [node['square'] for node in squareTree.values()]
-   pointlist = squaresToPoints(squares)
-   strokes =  pointsToStrokes(pointlist)
-   """
-   return strokes
-
-def pointsToStrokes(points):
-   """Take in an unordered list of points and convert it into one or more strokes"""
-   global DEBUGIMG 
-   retStroke = Stroke()
-   distances = {}
-
-   edgecount = 0
-   for i, p1 in enumerate(points):
-      for j, p2 in enumerate(points):
-         if i != j:
-            pointdist = pointDist(p1, p2)
-            pointsOfDist = distances.setdefault(pointdist, [])
-            pointsOfDist.append((p1, p2))
-            edgecount += 1
-   #print "%s points, %s edges in matrix" % (len(points), edgecount)
-
-   dists = distances.keys()
-   dists.sort()
-
-   
-   curPoints = {}
-   for d in dists:
-      for edge in distances[d]:
-         p1, p2 = edge
-         if ( 
-             (p1 in curPoints or p2 in curPoints) \
-             and not (p1 in curPoints and p2 in curPoints) \
-            ) \
-            or len(curPoints) == 0 : #First add
-            curPoints.setdefault(p1, []).append(p2) #Append p2 as a neighbor of p1
-            curPoints.setdefault(p2, []).append(p1) #P1 as a neighbor of p2
-      if len(curPoints) == len(points):
-         print "Got all the points at distance %s" % (d)
-         break
-
-   """
-   print "%s points in MST" % (len(curPoints.keys()))
-   for p1 in curPoints.keys():
-      for p2 in curPoints[p1]:
-         cv.Line(DEBUGIMG, p1, p2, 0x0, thickness=1)
-
-   show(DEBUGIMG)
-   
-
-### BREAAAAKK ###
-   """
-
-   #curPoints is now the MST
-
-
-   #Find a good leaf
-   maxLeaf = curPoints.keys()[0]
-   maxDist = 0
-   pointStack = [(maxLeaf, 0)]
-   seen = []
-   while len(pointStack) > 0:
-      top, curlevel = pointStack.pop()
-      if curlevel > maxDist:
-         maxDist = curlevel
-         maxLeaf = top
-      seen.append(top)
-      for kid in curPoints[top]:
-         if kid not in seen:
-            pointStack.append( (kid, curlevel+1) )
-
-
-
-   pointlist = []
-   if len(curPoints) > 0:
-      pointStack = [ maxLeaf ]
-      while len(pointStack)> 0:
-         print pointStack
-         top = pointStack.pop()
-         #print "Adding point %s" % (str(top))
-         #print "  Checking kids: %s" % (curPoints[top])
-         pointlist.append(top) #Random root
-         for kid in curPoints[top]:
-            if kid not in pointlist:
-               pointStack.append(kid) #Add the children
-
-   for p in pointlist:
-      retStroke.addPoint(p)
-
-   return [retStroke]
-
-def squaresToPoints(squares):
-   """Take in a list of squares, find a set of the biggest squares that approximates
-   the blob to a threshold accuracy, and convert the squares to a list of those squares'
-   center points.
-   Each square is a tuple of topleft, bottom right (x,y) point tuples: ( (tlx, tly), (brx, bry) )
-   """
-   errorThreshold = 0.1
-   retStrokes = []
-   pointList = []
-   sizeSqrs = {}
-   totalSize = 0
-   #Sort squares into descending order of size
-   for sqr in squares:
-      (p1x, p1y), (p2x, p2y) = sqr
-      size = (p2x - p1x)
-      totalSize += size * size #Add all of this squares pixes to total size
-      sizelist = sizeSqrs.setdefault(size, [])
-      sizelist.append(sqr)
-   sizes = sizeSqrs.keys()
-   sizes.sort(key=(lambda x: -x)) #sort descending
-
-   #Add progressively smaller squares until blob is approximately present
-   absTarget = totalSize - errorThreshold * totalSize #how many pixels do we need to fill to be "good enough"
-   currentPixels = 0
-   isDone = False
-   for size in sizes:
-      for sqr in sizeSqrs[size]:
-         (p1x, p1y), (p2x, p2y) = sqr 
-         center = ( (p1x + p2x)/2, (p1y + p2y) /2)
-         #retStroke.addPoint(center)
-         pointList.append(center)
-
-         currentPixels += size * size
-         if currentPixels > absTarget:
-            isDone = True
-            break
-            #return pointList 
-      if isDone:
-         break
-
-   return pointList
-
-
-def OrderStrokePoints(stroke):
-   """Take an stroke with an arbitrary order on its points and 
-   order them such that they could be drawn in a single gesture"""
-   retStroke = Stroke()
-   points = stroke.getPoints()
-
-   if len(points) == 0:
-      return retStroke
-
-   pointTree = {}
-   for i in range(0, len(points)):
-      minDist = None
-      minPoint = None
-      for j in range(0, len(points)):
-         if i == j: #Don't measure against myself
-            continue
-         curDist = pointDist(points[i], points[j])
-         if minDist is None or minDist < curDist:
-            minDist = curDist
-            minPoint = points[j]
-      iNode = pointTree.setdefault(points[i], {})
-      jNode = pointTree.setdefault(points[j], {})
-      iNode['next'] = points[j]
-
-def bitmapToUnorderedStrokes(img,step = 1):
+def bitmapToStrokes(img,step = 1):
    """Take in a binary image and group blobs of black into strokes.
    Point ordering of these strokes is undefined"""
+   global DEBUGIMG
    retStrokes = []
    print img.cols
    for i in range (0, img.cols, step):
@@ -550,8 +438,8 @@ def bitmapToUnorderedStrokes(img,step = 1):
             blobSquareTree = fillSquares(p, img, 255)
             blobSquares = []
             for center, squareNode in blobSquareTree.items():
-               blobSquares.append(squareNode['square'])
-            retStrokes.extend(squaresToStrokes(blobSquareTree))
+               size = squareNode['size']
+            retStrokes.extend(debugSquareTreeToStrokes(blobSquareTree))
 
    print "\rFound %s strokes                 " % (len(retStrokes))
    return retStrokes
@@ -566,11 +454,17 @@ def show(cv_img):
    Image.fromstring("L", cv.GetSize(cv_img), cv_img.tostring()).show()
    
 def main(args):
+   global SQUARE_ERROR, PRUNING_FACTOR
    if len (args) < 2:
-      print( "Usage: %s <image_file> [output_file]" % (args[0]))
+      print( "Usage: %s <image_file> [output_file] [pruning_factor] [square_error]" % (args[0]))
       exit(1)
 
    fname = args[1]
+   if len(args) > 4:
+      SQUARE_ERROR = float(args[4])
+   if len(args) > 3:
+      PRUNING_FACTOR = float(args[3])
+
    if len(args) > 2:
       outfname = args[2]
    else:
@@ -587,56 +481,6 @@ def main(args):
 
 
 
-def tester(args):
-   import pdb
-
-   print """
-           #0  1  2  3  4
-   IMG = [ [1, 1, 1, 1, 1], #0
-           [0, 1, 1, 1, 1], #1
-           [1, 1, 1, 0, 1], #2
-           [1, 1, 1, 1, 1], #3
-           [1, 1, 1, 1, 1], #4
-          ]
-   """
-           #0  1  2  3  4
-   IMG = [ [1, 1, 1, 1, 1], #0
-           [0, 1, 1, 1, 1], #1
-           [1, 1, 1, 0, 1], #2
-           [1, 1, 1, 1, 1], #3
-           [1, 1, 1, 1, 1], #4
-          ]
-
-
-   height = len(IMG)
-   width = len(IMG[0])
-
-   startpt = (random.randint(0,width-1), random.randint(0,height-1))
-   while not isValidPoint(startpt, IMG):
-      startpt = (random.randint(0,width-1), random.randint(0,height-1))
-
-
-   print "FillSquares"
-   squares = fillSquares(startpt, IMG)
-   
-   print "List of squares"
-   for i, s in enumerate(squares):
-      print "  Square %s: %s" % (i, s)
-
-   print "Filled image"
-   for row in IMG:
-      print row
-   
-   """
-
-   print "growSquare((1,1))"
-   print growSquare((1,1))
-
-   print "growSquare((1,0))"
-   print growSquare((0,2))
-   """
-
 if __name__ == "__main__":
    import sys
    main(sys.argv)
-   #tester(sys.argv)
