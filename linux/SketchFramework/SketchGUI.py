d681f566d4734f99fba718f8d264ee87ed18773c
Jeff Browne <jbrowne@cs.ucsb.edu>
2011-11-18 10:56:50 -0800

    Whoops, last commit just breaks. THIS ONE gets the correct standalone code going

--- linux/SketchFramework/SketchGUI.py
+++ linux/SketchFramework/SketchGUI.py
@@ -69,7 +69,8 @@ class _SketchGUI(object):
     
 def SketchGUISingleton():
     "Returns the GUI instance we're currently working with."
-    from SketchFramework import WpfSketchGUI as GuiInstance
+    #from SketchFramework import WpfSketchGUI as GuiInstance
+    from SketchFramework import TkSketchGUI as GuiInstance
     #from SketchFramework import NetSketchGUI as GuiInstance
     
     
